49d9a81ea1186468d114bde9dd1ce0fd8e828b99
Otavio <otaviomota19@hotmail.com>
2017-10-25 15:22:39 -0300

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,13 @@
 var socket = io();
 
 socket.on('connect', function (){
-  console.log('Connected to Server')
-
-  socket.emit('createMessage',{
-    from: 'Andrew',
-    text:"holaaaaaa"
-  })
+  console.log('Connected to Server');
 })
 
 socket.on('disconnect',function () {
-  console.log('Disconnected from server')
-})
+  console.log('Disconnected from server');
+});
 
 socket.on('newMessage', function(message){
-  console.log("newMessage",message)
-})
+  console.log("newMessage",message);
+});
--- server/server.js
+++ server/server.js
@@ -14,19 +14,20 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from:"John",
-    text:"See u then",
-    createAt:123456
-  })
 
   socket.on('createMessage', (message) => {
-    console.log('createMessage',message)
+    console.log('createMessage', message);
+    io.emit('newMessage',{
+      from:message.from,
+      text:message.text,
+      createAt: new Date().getTime()
+    })
   })
 
   socket.on('createEmail', (newEmail) => {
     console.log('createEmail',newEmail);
   })
+
   socket.on('disconnect', () => {
     console.log('User was disconnected')
   })
